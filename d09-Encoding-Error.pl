11074af28ae12e7b53673bb5f4efa0beb2736f85
Gustaf Erikson <gerikson@gmail.com>
2020-12-09 09:20:02 +0100

    cleaned up, added comments

--- d09-Encoding-Error.pl
+++ d09-Encoding-Error.pl
@@ -32,51 +32,45 @@ for ($lower..$upper) {
 OUTER:
 while ($upper < scalar @stream) {
 
-#    say join(" ", map {$stream[$_]} ($lower..$upper));
-#    say "target: $target";
     my @tests;
     for my $i ($lower..$upper) {
 	push @tests, $target-$stream[$i];
     }
-#    say join(" ",@tests);
     my $found = 0;
     foreach (@tests) {
 	$found++ if exists $h{$_};
     }
-#    say $found;
-    if (!$found) { # test for equality here
-#	say "==> $target";
+    if (!$found) {
+	# we've found the target, break out of the loop
 	last OUTER;
-    } 
-	delete $h{$stream[$lower]};
-	$h{$stream[$upper+1]}++;
-	$target = $stream[$upper+2];
-	$lower++;
-	$upper++;
-
+    }
+    # move our window
+    delete $h{$stream[$lower]};
+    $h{$stream[$upper+1]}++;
+    $target = $stream[$upper+2];
+    $lower++;
+    $upper++;
 }
 is( $target,20874512 ,"Part 1: $target");
+
 # part 2
 my $part2;
 # let's go from the top
 my $start = $#stream;
 while ($start>0) {
- #   say "$start";
     if ($stream[$start]>=$target) {
 	$start--;
 	next;
     }
-#    next if $stream[$start]>=$target;
+
     my $sum = $stream[$start];
     my $next = $start-1;
     while ($sum<$target) {
-#	say "$next $sum";
 	$sum +=$stream[$next];
 	$next--;
-	
     }
-  #  say "   $sum";
-    if ($sum==$target) {
+
+    if ($sum==$target) { # this could probably be more elegant
 	# find smallest and largest
 	my  $min = 2 * $stream[$#stream];
 	my $max = -1;
@@ -84,9 +78,6 @@ while ($start>0) {
 	    if ($stream[$_]<$min ) { $min=$stream[$_]}
 	    if ($stream[$_]>$max) { $max= $stream[$_]}
 	}
-#	say "$min $max ", sum($min,$max);
-#	say "==> ", join(' ', $start, $next+1, $stream[$start], $stream[$next+1]); 
-	#	say "==> $start $next ",$stream[$start-1]+$stream[$next];
 	$part2 = sum($min,$max);
 	last;
     }
