9511260b289003a30019c9ec1b2fbda0e95e7112
Gustaf Erikson <gerikson@gmail.com>
2020-12-18 10:27:42 +0100

    new RPN implementation, perltidy

--- d18-Operation-Order.pl
+++ d18-Operation-Order.pl
@@ -5,13 +5,14 @@
 #      License: http://gerikson.com/files/AoC2020/UNLICENSE
 ###########################################################
 use Modern::Perl '2015';
+
 # useful modules
 use Test::More;
 use Time::HiRes qw/gettimeofday tv_interval/;
 
 my $start_time = [gettimeofday];
 #### INIT - load input data from file into array
-my $part2 = shift//0;
+my $part2 = shift // 0;
 my @file_contents;
 my $file = 'input.txt';
 open( my $fh, '<', "$file" );
@@ -19,93 +20,95 @@ while (<$fh>) { chomp; s/\r//gm; push @file_contents, $_; }
 
 ### CODE
 # shunting yard algo adapted from http://www.rosettacode.org/wiki/Parsing/RPN_calculator_algorithm#Perl
-my %prec = ( '*' => 2, '+'=>2, '(' => 1 );
+my %prec = ( '*' => 2, '+' => 2, '(' => 1 );
 if ($part2) {
-    $prec{'+'} = 3
+    $prec{'+'} = 3;
 }
-my %assoc =( '*' => 'left', '+' => 'left' );
+my %assoc = ( '*' => 'left', '+' => 'left' );
 
 sub shunting_yard {
-    my @inp = split(//, $_[0]);
+    my @inp = split( //, $_[0] );
     my @ops;
     my @res;
-    my $report = sub {printf("%25s    %-7s %10s %s\n", "@res", "@ops", $_[0], "@inp")};
-    
-    my $shift = sub { #$report->("shift @_");
-		      push @ops, @_ };
-    my $reduce = sub { #$report->("reduce @_");
-		       push @res, @_ };
+    my $shift  = sub { push @ops, @_ };
+    my $reduce = sub { push @res, @_ };
 
     while (@inp) {
-	
-	my $token = shift @inp;
-	next if $token =~ /\s+/;
-	if ($token =~ /\d+/ ) { $reduce->( $token )}
-	elsif ($token eq '(' ) { $shift->( $token ) }
-	elsif ($token eq ')' ) {
-	    while (@ops and "(" ne ( my $x = pop @ops)) { $reduce->( $x )}
+        my $token = shift @inp;
+        next if $token =~ /\s+/;
+        if    ( $token =~ /\d+/ ) { $reduce->($token) }
+        elsif ( $token eq '(' )   { $shift->($token) }
+        elsif ( $token eq ')' ) {
+            while ( @ops and "(" ne ( my $x = pop @ops ) ) { $reduce->($x) }
 
-	} else {
-	    my $newprec = $prec{$token};
-	    while (@ops) {
-		my $oldprec = $prec{ $ops[-1] };
-		last if $newprec > $oldprec;
-		last if $newprec == $oldprec and $assoc{$token} eq 'right';
-		$reduce->( pop @ops );
-	    }
-	    $shift->($token);
-	}
+        }
+        else {
+            my $newprec = $prec{$token};
+            while (@ops) {
+                my $oldprec = $prec{ $ops[-1] };
+                last if $newprec > $oldprec;
+                last if $newprec == $oldprec and $assoc{$token} eq 'right';
+                $reduce->( pop @ops );
+            }
+            $shift->($token);
+        }
     }
-    $reduce->(pop @ops) while @ops;
-    return join(' ',@res);
+    $reduce->( pop @ops ) while @ops;
+    return join( ' ', @res );
 }
 
-# RPN parser from http://www.rosettacode.org/wiki/Parsing/RPN_calculator_algorithm#Perl
-sub evaluate {
-  (my $a = "($+{left})$+{op}($+{right})") =~ s/\^/**/;
-#  say $a;
-  eval $a;
-}
+# RPN solution from https://www.perlmonks.org/?node_id=520826
 sub RPN {
-    my @input =  $_[0];
-    my $number='[+-]?(?:\.\d+|\d+(?:\.\d*)?)';
-    my $operator='[-+*/^]';
-    for (@input) {
-	while (
-	       s/ \s* ((?<left>$number))     # 1st operand
-           \s+ ((?<right>$number))    # 2nd operand
-           \s+ ((?<op>$operator))     # operator
-           (?:\s+|$)                  # more to parse, or done?
-         /
-           ' '.evaluate().' '         # substitute results of evaluation
-	       /ex
-	      ) {}
-	return $_;
+    my ($in) = @_;
+    my @stack;
+    for my $tok ( split( ' ', $in ) ) {
+        if ( $tok =~ /\d+/ ) {
+            push @stack, $tok;
+            next;
+        }
+        my $x = pop @stack;
+        my $y = pop @stack;
+        if ( $tok eq '+' ) {
+            push @stack, $y + $x;
+        }
+        elsif ( $tok eq '*' ) {
+            push @stack, $y * $x;
+        }
+        else {
+            die "invalid token:\"$tok\"\n";
+        }
     }
+    @stack == 1 or die "invalid stack: [@stack]\n";
+    return $stack[0];
 }
+
 foreach (<DATA>) {
-    my ( $expr,$test1,$test2) = split ':';
+    my ( $expr, $test1, $test2 ) = split ':';
     my $res = shunting_yard($expr);
-    my $val = RPN( $res );
+    my $val = RPN($res);
     if ($part2) {
-	ok( $val==$test2)
-    } else {
-	ok($val==$test1)
+        ok( $val == $test2 );
+    }
+    else {
+        ok( $val == $test1 );
     }
 }
 
-my $sum =0;
+my $sum = 0;
 foreach my $expr (@file_contents) {
-    my $res= shunting_yard($expr);
+    my $res = shunting_yard($expr);
+
     #    say join(' ',$expr, '==>', $res);
-    my $val =RPN($res);
-#    say $val;
-    $sum+=$val;
+    my $val = RPN($res);
+
+    #    say $val;
+    $sum += $val;
 }
 if ($part2) {
-    is( $sum, 388966573054664,"Part2: " .$sum)
-} else {
-    is( $sum, 18213007238947,"Part1: " .$sum)
+    is( $sum, 388966573054664, "Part2: " . $sum );
+}
+else {
+    is( $sum, 18213007238947, "Part1: " . $sum );
 }
 
 done_testing();
