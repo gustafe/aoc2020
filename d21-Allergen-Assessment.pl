17016a43fa28cabbdc0d5b1d3d881b60bf166a6f
Gustaf Erikson <gerikson@gmail.com>
2020-12-21 16:34:51 +0100

    perltidy

--- d21-Allergen-Assessment.pl
+++ d21-Allergen-Assessment.pl
@@ -5,8 +5,9 @@
 #      License: http://gerikson.com/files/AoC2020/UNLICENSE
 ###########################################################
 use Modern::Perl '2015';
+
 # useful modules
-use Test::More tests=>2; 
+use Test::More tests => 2;
 use Time::HiRes qw/gettimeofday tv_interval/;
 
 my $start_time = [gettimeofday];
@@ -21,43 +22,46 @@ while (<$fh>) { chomp; s/\r//gm; push @file_contents, $_; }
 my %ingredients;
 my %allergens;
 foreach my $line (@file_contents) {
-    my @ins; my @alls;
-    if ($line =~ m/^(.*) \(contains (.*)\)$/) {
-	@ins = split(/ /, $1);
-	@alls = split(',', $2);
-	map { s/^\s+|\s+$//g } @alls;
+    my @ins;
+    my @alls;
+    if ( $line =~ m/^(.*) \(contains (.*)\)$/ ) {
+        @ins  = split( / /, $1 );
+        @alls = split( ',', $2 );
+        map {s/^\s+|\s+$//g} @alls;
 
-	foreach my $in (@ins) {
-	    $ingredients{$in}->{num}++;
-	}
-	foreach my $al (@alls) {
-	    $allergens{$al}->{num}++;
-	    foreach my $i (@ins) {
-		$allergens{$al}->{counts}->{$i}++
-	    }
-	}
-    } else {
-	die "can't parse: $line\n";
+        foreach my $in (@ins) {
+            $ingredients{$in}->{num}++;
+        }
+        foreach my $al (@alls) {
+            $allergens{$al}->{num}++;
+            foreach my $i (@ins) {
+                $allergens{$al}->{counts}->{$i}++;
+            }
+        }
+    }
+    else {
+        die "can't parse: $line\n";
     }
 }
 my %possibles;
-foreach my $al (keys %allergens) {
-    foreach my $in (keys %{$allergens{$al}->{counts}}) {
-	if ($allergens{$al}->{counts}->{$in} == $allergens{$al}->{num}) {
-	    $possibles{$in}->{$al}++
-	}
+foreach my $al ( keys %allergens ) {
+    foreach my $in ( keys %{ $allergens{$al}->{counts} } ) {
+        if ( $allergens{$al}->{counts}->{$in} == $allergens{$al}->{num} ) {
+            $possibles{$in}->{$al}++;
+        }
     }
 }
 
 my $part1 = 0;
-foreach my $in (keys %ingredients) {
-    $part1 += $ingredients{$in}->{num} unless exists $possibles{$in}
+foreach my $in ( keys %ingredients ) {
+    $part1 += $ingredients{$in}->{num} unless exists $possibles{$in};
 }
-is ($part1, 2461,"Part 1: ". $part1);
+is( $part1, 2461, "Part 1: " . $part1 );
 
 # part 2
 
 my %solution;
+
 # this part cribbed from 2018D16 and 2020D16
 while ( keys %possibles ) {
     foreach my $ing ( keys %possibles ) {
@@ -79,10 +83,11 @@ while ( keys %possibles ) {
         }
     }
 }
-my @part2 =();
-foreach my $in (sort {$solution{$a} cmp $solution{$b}} keys %solution) {
-    push @part2, $in
+my @part2 = ();
+foreach my $in ( sort { $solution{$a} cmp $solution{$b} } keys %solution ) {
+    push @part2, $in;
 }
-my $answer_str = join(',',@part2);
-is ( $answer_str, 'ltbj,nrfmm,pvhcsn,jxbnb,chpdjkf,jtqt,zzkq,jqnhd', "Part 2: ok");
+my $answer_str = join( ',', @part2 );
+is( $answer_str, 'ltbj,nrfmm,pvhcsn,jxbnb,chpdjkf,jtqt,zzkq,jqnhd',
+    "Part 2: ok" );
 say "Duration: ", tv_interval($start_time) * 1000, "ms";
