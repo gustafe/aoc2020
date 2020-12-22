eb3ead79300f13c9236d46509d18fbae9036843b
Gustaf Erikson <gerikson@gmail.com>
2020-12-22 14:39:25 +0100

    cleanup, perltidy

--- d22-Crab-Combat.pl
+++ d22-Crab-Combat.pl
@@ -5,10 +5,10 @@
 #      License: http://gerikson.com/files/AoC2020/UNLICENSE
 ###########################################################
 use Modern::Perl '2015';
+
 # useful modules
-use List::Util qw/sum any all max/;
-use Data::Dump qw/dump/;
-use Test::More tests=>2;
+use List::Util qw/max/;
+use Test::More tests => 2;
 use Time::HiRes qw/gettimeofday tv_interval/;
 
 my $start_time = [gettimeofday];
@@ -18,156 +18,114 @@ my $testing = 0;
 my @file_contents;
 my $file = $testing ? 'test.txt' : 'input.txt';
 open( my $fh, '<', "$file" );
-{ local $/='';
-  while (<$fh>) {
-      chomp;
-      push @file_contents, $_;
-  }
+{
+    local $/ = '';
+    while (<$fh>) {
+        chomp;
+        push @file_contents, $_;
+    }
 }
 ### SUBS
 
 sub score {
-    my ( $d ) = @_;
+    my ($d) = @_;
     return 0 unless @$d;
-    my $score=0;
+    my $score  = 0;
     my $factor = 1;
     while (@$d) {
-	$score += $factor * pop @$d;
-	$factor++;
+        $score += $factor * pop @$d;
+        $factor++;
     }
     return $score;
 }
 
 sub make_state {
     my ( $d0, $d1 ) = @_;
-    return join(',',@$d0).':'.join(',',@$d1);
+    return join( ',', @$d0 ) . ':' . join( ',', @$d1 );
 }
+
 # this code courtesy of /u/musifter at https://www.reddit.com/r/adventofcode/comments/khyjgv/2020_day_22_solutions/ggo7v7a/
 sub recurse_game {
     my ( $depth, $d0, $d1 ) = @_;
     my %states;
     $game_no++;
-    print "Starting game $game_no at depth $depth\r" if ( $game_no % 100 == 0);
+    print "Starting game $game_no at depth $depth\r"
+        if ( $game_no % 100 == 0 );
 
-    while (@$d0 and @$d1) {
-	my $curr_state = make_state( $d0, $d1 );
-	if (exists $states{$curr_state}) {
-	    return ( ($depth>0) ? 0 : score( @$d0));
-	}
-	$states{$curr_state}++;
+    while ( @$d0 and @$d1 ) {
+        my $curr_state = make_state( $d0, $d1 );
+        if ( exists $states{$curr_state} ) {
+            return ( ( $depth > 0 ) ? 0 : score(@$d0) );
+        }
+        $states{$curr_state}++;
 
-	my $c0 = shift @$d0;
-	my $c1 = shift @$d1;
+        my $c0 = shift @$d0;
+        my $c1 = shift @$d1;
 
-	my $winner;
-	if (($c0 <= scalar @$d0) and $c1 <= scalar @$d1) {
-	    my $new_d0 = [map {$d0->[$_]} (0..$c0-1)];
-	    my $new_d1 = [map {$d1->[$_]} (0..$c1-1)];
-	    $winner = recurse_game( $depth +1, $new_d0, $new_d1);
-	} elsif ($c0>$c1) {
-	    $winner = 0;
-	} else {
-	    $winner=1;
-	}
-	if ($winner==0) {
-	    push @$d0, ($c0,$c1)
-	} else {
-	    push @$d1, ($c1,$c0)
-	}
+        my $winner;
+        if ( ( $c0 <= scalar @$d0 ) and $c1 <= scalar @$d1 ) {
+            my $new_d0 = [ map { $d0->[$_] } ( 0 .. $c0 - 1 ) ];
+            my $new_d1 = [ map { $d1->[$_] } ( 0 .. $c1 - 1 ) ];
+            $winner = recurse_game( $depth + 1, $new_d0, $new_d1 );
+        }
+        elsif ( $c0 > $c1 ) {
+            $winner = 0;
+        }
+        else {
+            $winner = 1;
+        }
+        if ( $winner == 0 ) {
+            push @$d0, ( $c0, $c1 );
+        }
+        else {
+            push @$d1, ( $c1, $c0 );
+        }
     }
-    if ($depth>0) {
-	return (@$d0?0:1)
-    } else {
-	return max(score( $d0, $d1));
+    if ( $depth > 0 ) {
+        return ( @$d0 ? 0 : 1 );
     }
-    
+    else {
+        return max( score( $d0, $d1 ) );
+    }
+
 }
 
 ### CODE
 # part 1
 my @decks;
 foreach (@file_contents) {
-    my @data = split("\n", $_);
+    my @data = split( "\n", $_ );
     shift @data;
     push @decks, [@data];
 }
 
 foreach (@decks) {
-    say join(',', map{sprintf("%2d",$_)} ($_->@*));
+    say join( ',', map { sprintf( "%2d", $_ ) } ( $_->@* ) );
 }
-while ($decks[0]->@* and $decks[1]->@*) {
+while ( $decks[0]->@* and $decks[1]->@* ) {
     my $c0 = shift $decks[0]->@*;
     my $c1 = shift $decks[1]->@*;
 
-    if ($c0 > $c1) {
-	push $decks[0]->@*, ($c0, $c1);
-    } else {
-	push $decks[1]->@*, ($c1, $c0);
+    if ( $c0 > $c1 ) {
+        push $decks[0]->@*, ( $c0, $c1 );
+    }
+    else {
+        push $decks[1]->@*, ( $c1, $c0 );
     }
 }
-my $part1 = max( map{score( $_ )} @decks);
-is($part1,33561 ,"Part 1: ".$part1);
+my $part1 = max( map { score($_) } @decks );
+is( $part1, 33561, "Part 1: " . $part1 );
 
-# part2 
+# part2
 # reload decks
-@decks=();
+@decks = ();
 foreach (@file_contents) {
-    my @data = split("\n", $_);
+    my @data = split( "\n", $_ );
     shift @data;
     push @decks, [@data];
 }
 my $part2 = recurse_game( 0, $decks[0], $decks[1] );
 print "\n";
-is($part2, 34594, "Part 2: ".$part2);
+is( $part2, 34594, "Part 2: " . $part2 );
 
 say "Duration: ", tv_interval($start_time) * 1000, "ms";
-__END__
-my $round = 1;
-PLAY: 
-while (1) {
-    say "==> $round";
-    for (0,1) {
-	say join(' ', @{$players[$_]->{deck}});
-    }
-    if (all { scalar $players[$_]->{deck}->@* >1 } (0,1) ) { # both have cards
-	my $d1 = shift @{$players[0]->{deck}};
-	my $d2 = shift @{$players[1]->{deck}};
-	if ($d1 > $d2 ) {
-	    say "    player 1 wins!";
-	    push @{$players[0]->{deck}}, ( $d1, $d2 );
-	} elsif ($d2> $d1) {
-	    say "    player 2 wins!";
-	    push @{$players[1]->{deck}}, ( $d2, $d1 );
-	} else {
-	    die "equal cards $d1 and $d2 - someone's cheating!!";
-	}
-    } elsif (any {scalar $players[$_]->{deck}->@*  == 1} (0,1)) {
-	# final round
-	my $d1 = shift @{$players[0]->{deck}};
-	my $d2 = shift @{$players[1]->{deck}};
-	if ($d1 > $d2 ) {
-	    say "    player 1 wins!";
-	    push @{$players[0]->{deck}}, ( $d1, $d2 );
-	} elsif ($d2> $d1) {
-	    say "    player 2 wins!";
-	    push @{$players[1]->{deck}}, ( $d2, $d1 );
-	}
-	last PLAY;
-    }
-
-    $round++;
-}
-say dump \@players;
-foreach (0,1) {
-    next if scalar @players[$_]->{deck} == 0;
-    my $factor = 1;
-    my $sum = 0;
-    foreach (reverse $players[$_]->{deck}->@*) {
-	$sum += $factor * $_;
-	$factor++;
-    }
-    say $sum;
-}
-
-
-
